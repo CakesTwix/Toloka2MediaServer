8dd3b4233c76d1edebce1b3c97c66dd71c2b1156
CakesTwix <oleg.kiryazov@gmail.com>
2023-08-24 12:12:51 +0300

    core: Reformat, Add check by Guid

--- toloka2transmission/__main__.py
+++ toloka2transmission/__main__.py
@@ -4,13 +4,10 @@ import configparser
 import logging
 import sys
 
-from transmission_rpc import Client
-from transmission_rpc.error import TransmissionConnectError
-from transmission_rpc.utils import format_size
-
-from toloka2transmission.config import app, titles
 from toloka2transmission.utils.general import get_numbers
-from toloka2transmission.utils.transmission import search
+from toloka2transmission.utils.transmission import search, update
+from toloka2transmission.transmission import TransmissionClient
+from toloka2transmission.config import titles, app
 
 # Instantiate the parser
 parser = argparse.ArgumentParser(
@@ -21,7 +18,12 @@ parser = argparse.ArgumentParser(
 parser.add_argument("-c", "--codename", type=str, help="Коднейм тайтла")
 parser.add_argument("-n", "--num", type=str, help="Отримати список чисел з рядка")
 parser.add_argument("-a", "--add", type=str, help="Додати нове аніме в конфіг.")
-
+parser.add_argument(
+    "-f",
+    "--force",
+    action=argparse.BooleanOptionalAction,
+    help="Примусово качати торрент, не дивлячись на його відсутність.",
+)
 
 args = parser.parse_args()
 
@@ -31,45 +33,21 @@ if args.num:
     sys.exit()
 
 # Set Logging
-logging.basicConfig(level=logging.INFO)
-
-# Init
-try:
-    TransmissionClient = Client(
-        host=app["Transmission"]["host"],
-        port=app["Transmission"]["port"],
-        username=app["Transmission"]["username"],
-        password=app["Transmission"]["password"],
-        path=app["Transmission"]["rpc"],
-        protocol=app["Transmission"]["protocol"],
-    )
-    # For init information
-    downloaded = format_size(
-        TransmissionClient.session_stats().current_stats.downloaded_bytes
-    )
-    uploaded = format_size(
-        TransmissionClient.session_stats().current_stats.uploaded_bytes
-    )
-
-    logging.info(f"Downloaded: {downloaded[0]} {downloaded[1]}")
-    logging.info(f"Uploaded: {uploaded[0]} {uploaded[1]}")
-except TransmissionConnectError:
-    logging.critical("Not connection!")
-    sys.exit()
+logging.basicConfig(level=app["Python"]["logging"])
 
 # Add new anime to titles.ini
 if args.add:
-    torrent = search(args.add)["Results"][0]
-    if (
-        input(f"Воно? 0 - ні, інаше - так:\n {torrent['Title']} - {torrent['Guid']}")
-        == "0"
-    ):
-        sys.exit()
+    torrent = search(args.add)["Results"]
+    for index, item in enumerate(torrent[:10]):
+        print(f"{index} : {item['Title']} - {item['Guid']}")
+
+    torrent = torrent[int(input("Введіть номер потрібного торрента: "))]
 
     codename = input("Enter the codename: ")
     config_update = configparser.RawConfigParser()
     config_update.add_section(codename)
     config_update.set(codename, "name", args.add)
+    config_update.set(codename, "Guid", torrent["Guid"])
 
     # Get data
     season_number = input("Введіть номер сезону: ")
@@ -87,7 +65,9 @@ if args.add:
     )
 
     episode_number = int(
-        input(f"Введіть індекс серії\n{new_torrent.get_files()[0].name} : {get_numbers(new_torrent.get_files()[0].name)}: ")
+        input(
+            f"Введіть індекс серії\n{new_torrent.get_files()[0].name} : {get_numbers(new_torrent.get_files()[0].name)}: "
+        )
     )
 
     # Write data
@@ -98,7 +78,7 @@ if args.add:
     config_update.set(codename, "download_dir", download_dir)
     config_update.set(codename, "publishdate", torrent["PublishDate"])
 
-    for name_id, name in enumerate(new_torrent.get_files()):
+    for name in new_torrent.get_files():
         logging.info(name.name)
         # Episode S1E01.mkv
         new_name = f"Episode S{season_number}E{get_numbers(name.name)[episode_number]}{ext_name}"
@@ -119,117 +99,9 @@ if args.add:
 
 # Update this anime
 if args.codename:
-    # All ok, do magic ^_^
-    for torrent in TransmissionClient.get_torrents():
-        if titles[args.codename]["torrent_name"] == torrent.name:
-            logging.debug(torrent.name)
-
-            toloka = search(titles[args.codename]["name"])
-            logging.info(toloka["Results"][0]["Guid"])
-            if input("Воно? 0 - ні, інаше - так:\n") == "0":
-                sys.exit()
-
-            # Check if have updates by date
-            if (
-                titles[args.codename]["PublishDate"]
-                == toloka["Results"][0]["PublishDate"]
-            ):
-                logging.info("Same date!")
-                sys.exit()
-            else:
-                logging.info("Date is different!")
-
-                # Update date and write
-                titles[args.codename]["PublishDate"] = toloka["Results"][0][
-                    "PublishDate"
-                ]
-                with open("titles.ini", "w", encoding="utf-8") as conf:
-                    titles.write(conf)
-
-                # Remove old torrent
-                TransmissionClient.remove_torrent(torrent.id)
-
-                # Download torrent file
-                new_torrent = TransmissionClient.get_torrent(
-                    TransmissionClient.add_torrent(
-                        toloka["Results"][0]["Link"],
-                        download_dir=titles[args.codename]["download_dir"],
-                    ).id
-                )
-
-                # Rename episodes
-                if titles[args.codename]["episode_number"]:
-                    # New torrent Files
-                    for name_id, name in enumerate(new_torrent.get_files()):
-                        logging.info(name.name)
-                        # Episode S1E01.mkv
-                        new_name = f"Episode S{titles[args.codename]['season_number']}E{get_numbers(name.name)[int(titles[args.codename]['episode_number'])]}{titles[args.codename]['ext_name']}"
-                        TransmissionClient.rename_torrent_path(
-                            new_torrent.id, name.name, new_name
-                        )
-
-                # Rename Torrent
-                TransmissionClient.rename_torrent_path(
-                    new_torrent.id,
-                    new_torrent.name,
-                    titles[args.codename]["torrent_name"],
-                )
-
-                # Check old files
-                TransmissionClient.verify_torrent(new_torrent.id)
-                TransmissionClient.start_torrent(new_torrent.id)
+    update(args.codename, args.force)
+    sys.exit()
 
 # Update all titles
 for title in titles.sections():
-    # All ok, do magic ^_^
-    for torrent in TransmissionClient.get_torrents():
-        if titles[title]["torrent_name"] == torrent.name:
-            logging.debug(torrent.name)
-
-            toloka = search(titles[title]["name"])
-            logging.info(toloka["Results"][0]["Guid"])
-            if input("Воно? 0 - ні, інаше - так:\n") == "0":
-                break
-
-            # Check if have updates by date
-            if titles[title]["PublishDate"] == toloka["Results"][0]["PublishDate"]:
-                logging.info("Same date!")
-                break
-            else:
-                logging.info("Date is different!")
-
-                # Update date and write
-                titles[title]["PublishDate"] = toloka["Results"][0]["PublishDate"]
-                with open("titles.ini", "w", encoding="utf-8") as conf:
-                    titles.write(conf)
-
-                # Remove old torrent
-                TransmissionClient.remove_torrent(torrent.id)
-
-                # Download torrent file
-                new_torrent = TransmissionClient.get_torrent(
-                    TransmissionClient.add_torrent(
-                        toloka["Results"][0]["Link"],
-                        download_dir=titles[title]["download_dir"],
-                    ).id
-                )
-
-                # Rename episodes
-                if titles[title]["episode_number"]:
-                    # New torrent Files
-                    for name_id, name in enumerate(new_torrent.get_files()):
-                        logging.info(name.name)
-                        # Episode S1E01.mkv
-                        new_name = f"Episode S{titles[title]['season_number']}E{get_numbers(name.name)[int(titles[title]['episode_number'])]}{titles[title]['ext_name']}"
-                        TransmissionClient.rename_torrent_path(
-                            new_torrent.id, name.name, new_name
-                        )
-
-                # Rename Torrent
-                TransmissionClient.rename_torrent_path(
-                    new_torrent.id, new_torrent.name, titles[title]["torrent_name"]
-                )
-
-                # Check old files
-                TransmissionClient.verify_torrent(new_torrent.id)
-                TransmissionClient.start_torrent(new_torrent.id)
+    update(title, args.force)
--- toloka2transmission/utils/transmission.py
+++ toloka2transmission/utils/transmission.py
@@ -1,6 +1,12 @@
 """Functions for working with torrents"""
+import logging
+import sys
+
 import requests
-from toloka2transmission.config import app
+
+from toloka2transmission.config import app, titles
+from toloka2transmission.transmission import TransmissionClient
+from toloka2transmission.utils.general import get_numbers
 
 
 def search(query):
@@ -9,3 +15,74 @@ def search(query):
         f'{app["Jackett"]["url"]}/api/v2.0/indexers/all/results?apikey={app["Jackett"]["api_key"]}&Query={query}&Tracker[]=toloka',
         timeout=10,
     ).json()
+
+
+def update(title: str, force: bool):
+    # All ok, do magic ^_^
+    # Search Anime by "search query" and check Guid
+    toloka = search(titles[title]["name"])["Results"]
+
+    for torrent in toloka[:10]:
+        if torrent["Guid"] == titles[title]["guid"]:
+            toloka = torrent
+            break
+
+    # Not found by guid
+    if isinstance(toloka, list):
+        logging.critical("Not found! Check Guid!")
+        sys.exit()
+
+    # Check if have updates by date
+    if titles[title]["PublishDate"] == toloka["PublishDate"]:
+        logging.info(f"Same date! : {toloka['Title']}")
+        return
+    else:
+        # We have some changes! Do redownload torrent
+        logging.info(f"Date is different! : {toloka['Title']}")
+
+        # Update date and write
+        titles[title]["PublishDate"] = toloka["PublishDate"]
+        with open("titles.ini", "w", encoding="utf-8") as conf:
+            titles.write(conf)
+
+    # Get all torrents and get one by name
+    for torrent in TransmissionClient.get_torrents():
+        if titles[title]["torrent_name"] == torrent.name or force:
+            # Remove old torrent
+            if not force:
+                TransmissionClient.remove_torrent(torrent.id)
+
+            # Download torrent file
+            new_torrent = TransmissionClient.get_torrent(
+                TransmissionClient.add_torrent(
+                    toloka["Link"],
+                    download_dir=titles[title]["download_dir"],
+                ).id
+            )
+
+            # Rename episodes
+            if titles[title]["episode_number"]:
+                # New torrent Files
+                for name in new_torrent.get_files():
+                    # Episode S1E01.mkv
+                    logging.debug(
+                        f"Episode S{titles[title]['season_number']}E{get_numbers(name.name)[int(titles[title]['episode_number'])]}{titles[title]['ext_name']}"
+                    )
+                    new_name = f"Episode S{titles[title]['season_number']}E{get_numbers(name.name)[int(titles[title]['episode_number'])]}{titles[title]['ext_name']}"
+                    TransmissionClient.rename_torrent_path(
+                        new_torrent.id, name.name, new_name
+                    )
+
+            # Rename Torrent
+            TransmissionClient.rename_torrent_path(
+                new_torrent.id,
+                new_torrent.name,
+                titles[title]["torrent_name"],
+            )
+
+            # Check old files
+            TransmissionClient.verify_torrent(new_torrent.id)
+            TransmissionClient.start_torrent(new_torrent.id)
+
+            # No need check next torrents
+            break
