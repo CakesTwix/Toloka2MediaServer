a19c02e9430a9be4992aaec859c524906c2d841f
maksii <1761348+maksii@users.noreply.github.com>
2024-05-30 12:59:01 +0300

    web app error handling and ui

--- toloka2MediaServerWeb/app.py
+++ toloka2MediaServerWeb/app.py
@@ -1,5 +1,5 @@
 import logging
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for, session
 import sys
 
 import toloka2MediaServer.main_logic
@@ -7,6 +7,8 @@ sys.path.insert(1, '../toloka2MediaServer')
 import toloka2MediaServer
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'  # Set this to a strong secret value
+
 logger = logging.basicConfig(
     filename='toloka2MediaServer/data/app_web.log',  # Name of the file where logs will be written
     filemode='a',  # Append mode, which will append the logs to the file if it exists
@@ -19,23 +21,27 @@ class RequestData:
     index: int = 0
     correction: int = 0
     title: str = ""
-    def __init__(self, url = "", season = 0, index = 0, correction = 0, title = ""):
+    codename: str = ""
+    force: bool = False
+    def __init__(self, url = "", season = 0, index = 0, correction = 0, title = "", codename = "", force=False):
         self.url = url
         self.season = season
         self.index = index
         self.correction = correction
         self.title = title
+        self.codename = codename
+        self.force = force
 
 
-@app.route('/', methods=['GET', 'POST'])
-
-
+@app.route('/', methods=['GET'])
 def index():
     titles = toloka2MediaServer.main_logic.titles
     # Creating a list of dictionaries, each containing the data for the selected keys
     data = []
+    codenames =[]
     keys = ['torrent_name', 'publish_date', 'guid']
     for section in titles.sections():
+        codenames.append(section)
         section_data = {'codename': section}
         for key in keys:
             section_data[key] = titles.get(section, key)
@@ -48,8 +54,13 @@ def index():
         'publish_date': 'Last Updated',
         'guid': 'URL'
     }
-        
-    if request.method == 'POST':
+    output = session.pop('output', None)   
+    return render_template('index.html', data=data, columns=columns, codenames=codenames, output=output)
+
+@app.route('/add_release', methods=['POST'])
+def add_release():
+    # Process the URL to add release
+    try:
         requestData = RequestData(
             url = request.form['url'],
             season = request.form['season'],
@@ -57,10 +68,40 @@ def index():
             correction = int(request.form['correction']),
             title = request.form['title'],
         )
-        
-        
+
+
         #--add --url https://toloka.to/t675888 --season 02 --index 2 --correction 0 --title "Tsukimichi -Moonlit Fantasy-"
-        
+
         output = toloka2MediaServer.main_logic.add_release_by_url(requestData, logger)
-        return render_template('result.html', output=output)
-    return render_template('index.html', data=data, columns=columns)
+        message = f'Release added from URL. {output}'
+    except Exception as e:
+        message = f'Error: {str(e)}'
+    session['output'] = message
+    return redirect(url_for('index'))
+
+@app.route('/update_release', methods=['POST'])
+def update_release():
+    # Process the name to update release
+    try:
+        requestData = RequestData(
+            codename = request.form['codename']
+        )
+        output = toloka2MediaServer.main_logic.update_release_by_name(requestData, requestData.codename, logger)
+        message = f'Release updated by name  {output}'
+    except Exception as e:
+        message = f'Error: {str(e)}'
+    session['output'] = message
+    return redirect(url_for('index'))
+
+@app.route('/update_all_releases', methods=['POST'])
+def update_all_releases():
+    # Process to update all releases
+    try:
+        requestData = RequestData()
+        output = toloka2MediaServer.main_logic.update_releases(requestData, logger)
+
+        message = f'All releases updated  {output}'
+    except Exception as e:
+        message = f'Error: {str(e)}'
+    session['output'] = message
+    return redirect(url_for('index'))
