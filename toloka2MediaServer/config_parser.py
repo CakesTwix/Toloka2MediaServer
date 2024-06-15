4e56fc4fc5068e8d6319c6db919330302aac6d68
maksii <1761348+maksii@users.noreply.github.com>
2024-06-15 19:48:34 +0300

    read\write configs always in utf-8

--- toloka2MediaServer/config_parser.py
+++ toloka2MediaServer/config_parser.py
@@ -14,8 +14,8 @@ def load_configurations(app_config_path=None, title_config_path=None):
 
     app_config = configparser.ConfigParser()
     titles_config = configparser.ConfigParser()
-    app_config.read(app_config_path)
-    titles_config.read(title_config_path)
+    app_config.read(app_config_path, encoding='utf-8')
+    titles_config.read(title_config_path, encoding='utf-8')
     application_config = config_to_app(app_config)
     return app_config, titles_config, application_config
 
@@ -32,9 +32,9 @@ def update_config(config, code_name, title_config_path=None):
     if title_config_path is None:
         title_config_path = os.path.join(os.getcwd(), 'data', 'titles.ini')
     config_file = configparser.ConfigParser()
-    config_file.read(title_config_path)
+    config_file.read(title_config_path, encoding='utf-8')
 
     config_file[code_name] = config[code_name]
 
-    with open(title_config_path, 'w') as ini:
+    with open(title_config_path, 'w', encoding='utf-8') as ini:
         config_file.write(ini)
